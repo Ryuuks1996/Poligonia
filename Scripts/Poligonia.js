bd344bdb6b26e72c3df7ef1b46585ade170ba3d4
Felipe Duran <ryuuks1996@gmail.com>
2019-06-27 17:45:29 -0400

    Add Ship3D

--- Scripts/Poligonia.js
+++ Scripts/Poligonia.js
@@ -4,6 +4,34 @@ init();
 
 var init = function()
 {
+	//SCENE
+	scene = new THREE.Scene();
+	
+	var WIDTH = window.innerWidth;
+	var HEIGHT = window.innerHeight;
+	
+	//RENDERER
+	renderer = new THREE.WebGLRenderer({antialias:true});
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setSize(WIDTH, HEIGHT);
+	document.body.appendChild(renderer.domElement);
+	
+	//CAMERA
+	camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 20000);
+	camera.position.set(0,0,30);
+	
+	scene.add(camera);
+	renderer.setClearColor(0x333F47, 1);
+	
+	//RESIZE RENDERER BY BROWSER WINDOW
+	window.addEventListener('resize', function()
+	{
+		var WIDTH = window.innerWidth,
+		HEIGHT = window.innerHeight;
+		renderer.setSize(WIDTH, HEIGHT);
+		camera.aspect = WIDTH / HEIGHT;
+		camera.updateProjectionMatrix();
+    });
 }
 
 var update = function()
