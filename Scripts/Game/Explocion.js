16cc4334a0b87ef543a1f8fe23f55e9163af6edc
Ryuuks1996 <44385392+Ryuuks1996@users.noreply.github.com>
2019-07-15 18:20:41 -0400

    Add Explosion

--- Scripts/Game/Explocion.js
+++ Scripts/Game/Explocion.js
@@ -1,7 +1,31 @@
-class Explocion
+class Explocion extends GameObject
 {
-	constructor()
+	constructor(x, y, z, obj3D, material, initialSize, finalSize)
 	{
-	
+		super(x, y, z, obj3D, material);
+		
+		this.initialSize = initialSize;
+		this.finalSize = finalSize;
+
+		this.time = new Time();
+	}
+
+	Update()
+	{
+		super.Update();
+
+		var value = THREE.Math.lerp(this.initialSize, this.finalSize, this.time.actualTime)
+		if(this.finalSize != value)
+		{
+			this.gameObject.mesh.scale.set(value,value,value);
+		}
+		else
+		{
+			for(var child in this.gameObject.mesh.children)
+			{
+				this.gameObject.mesh.children[child].material.transparent = true;
+			}
+			this.gameObject.destroyed = true;
+		}
 	}
 }
